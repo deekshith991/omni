7dd9271f0d853431b90222cb5d6499547d17dd60
Deekshith Vaggu <vaggudeekshith@gmail.com>
2026-02-06 22:04:29 +0530

    Basic metadata

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,15 @@ fn main() {
             .green()
             .bold()
     );
+
+    basic_data();
+}
+
+fn basic_data() {
+    let name = env!("CARGO_PKG_NAME");
+    let version = env!("CARGO_PKG_VERSION");
+    let authors = env!("CARGO_PKG_AUTHORS");
+
+    println!("{}", format!("{name} v{version}").blue());
+    println!("Author(s): {}", authors);
 }
